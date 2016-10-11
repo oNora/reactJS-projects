5ef213e65d07afcbfaca75e0965c9a90b9978a3d
Eleonora Dimchevska <e.dimchevska@gmail.com>
2016-10-11 13:41:48 +0300

    change hashHistory to browserHistory

--- reactJS-fundamentals/fundamentals-routes/app/config/routes.js
+++ reactJS-fundamentals/fundamentals-routes/app/config/routes.js
@@ -2,14 +2,15 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var hashHistory = ReactRouter.hashHistory;
+//var hashHistory = ReactRouter.hashHistory;
+var browserHistory = ReactRouter.browserHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var InnerPage = require("../components/InnerPage");
 
 var routes = (
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
         <Route path='/' component={Main}>
             <IndexRoute path='/home' component={Home} />
             <Route path='/page' component={InnerPage}> </Route>
