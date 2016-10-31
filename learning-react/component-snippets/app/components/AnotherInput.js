0870a2caf5adc333f83d3697717550787ef04bcd
Eleonora Dimchevska <e.dimchevska@gmail.com>
2016-10-31 13:30:47 +0200

    fixing usage of Refs

--- learning-react/component-snippets/app/components/AnotherInput.js
+++ learning-react/component-snippets/app/components/AnotherInput.js
@@ -4,15 +4,13 @@ var AnotherInput = React.createClass({
   handleClick: function() {
     // Explicitly focus the text input using the raw DOM API.
     if (this.myTextInput !== null) {
-      this.myTextInput.focus();
+      this.refs.myTextInput.focus();
     }
   },
   render: function() {
-    // The ref attribute is a callback that saves a reference to the
-    // component to this.myTextInput when the component is mounted.
     return (
       <div>
-        <input type="text" ref={(ref) => this.myTextInput = ref} />
+        <input type="text" ref="myTextInput" /
         <input
           type="button"
           value="Focus the text input"
